96e1bae7d897cfd88c0e00b0822da27f5b3829ef
AnnaBe <pereiradelima938@gmail.com>
2023-11-01 19:56:55 -0300

    Inserção de usuário com POST

--- src/main/java/com/annabea/workshop/config/Instantiation.java
+++ src/main/java/com/annabea/workshop/config/Instantiation.java
@@ -1,5 +1,7 @@
 package com.annabea.workshop.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +18,10 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {	
-	}
-	
 	User maria = new User(null, "Maria Brown", "maria@gmail.com");
 	User alex = new User(null, "Alex Green", "alex@gmail.com");
 	User bob = new User(null, "Bob Grey", "bob@gmail.com");
 	
 	userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
 }
