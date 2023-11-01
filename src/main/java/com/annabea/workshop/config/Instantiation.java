048d5e7529ccb7df29d7d11263b72dde7eb99713
AnnaBe <pereiradelima938@gmail.com>
2023-11-01 15:36:56 -0300

    Obtendo um usuário por id

--- src/main/java/com/annabea/workshop/config/Instantiation.java
+++ src/main/java/com/annabea/workshop/config/Instantiation.java
@@ -7,15 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import com.annabea.workshop.domain.User;
 import com.annabea.workshop.repositories.UserRepository;
 
+
 @Configuration
 public class Instantiation implements CommandLineRunner{
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
-
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {	
+	}
 	
 	User maria = new User(null, "Maria Brown", "maria@gmail.com");
 	User alex = new User(null, "Alex Green", "alex@gmail.com");
--- src/main/java/com/annabea/workshop/services/UserService.java
+++ src/main/java/com/annabea/workshop/services/UserService.java
@@ -1,12 +1,14 @@
 package com.annabea.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.annabea.workshop.domain.User;
 import com.annabea.workshop.repositories.UserRepository;
+import com.annabea.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
 }
