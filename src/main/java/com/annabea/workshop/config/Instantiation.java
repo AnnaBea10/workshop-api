ea5b499c1baf2eb488074eaa3f17edb9fd9225f9
AnnaBe <pereiradelima938@gmail.com>
2023-11-02 21:43:14 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/annabea/workshop/config/Instantiation.java
+++ src/main/java/com/annabea/workshop/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.annabea.workshop.domain.Post;
 import com.annabea.workshop.domain.User;
+import com.annabea.workshop.dto.AuthorDTO;
 import com.annabea.workshop.repositories.PostRepository;
 import com.annabea.workshop.repositories.UserRepository;
 
@@ -38,9 +39,8 @@ public class Instantiation implements CommandLineRunner{
 	
 	userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	
-	
-	Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-	Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+	Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+	Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
 	postRepository.saveAll(Arrays.asList(post1, post2));
 	}
